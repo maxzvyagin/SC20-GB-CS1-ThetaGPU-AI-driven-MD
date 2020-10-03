bf418edc0b3184237c6232e1245fa569a98f963e
msalim <msalim@anl.gov>
2020-10-03 17:33:25 -0500

    Update YAML schema; work on driver

--- sim/driver/config.py
+++ sim/driver/config.py
@@ -3,7 +3,11 @@
 import yaml
 from pydantic import BaseSettings
 
+
 class MDConfig(BaseSettings):
+    """
+    Auto-generates configuration file for run_openmm.py
+    """
     pdb_file: Path
     reference_pdb_file: Path
     top_file: Optional[Path]
@@ -18,32 +22,99 @@ class MDConfig(BaseSettings):
 
 
 class MDRunnerConfig(BaseSettings):
+    """
+    Global MD configuration (written one per experiment)
+    """
     num_jobs: int
     pdb_file: Path
     reference_pdb_file: Path
     top_file: Optional[Path]
-    checkpoint_file: Optional[Path]
     simulation_length_ns: int = 10
     report_interval_ps: int = 50
-    h5_scp_path: Optional[str]
-    h5_cp_path: Optional[str]
+
 
 class OutlierDetectionConfig(BaseSettings):
     num_jobs: int
 
+
 class GPUTrainingConfig(BaseSettings):
     pass
 
+
 class CS1TrainingConfig(BaseSettings):
-    pass
+    sim_data_dir: Optional[Path]
+    data_dir: Optional[Path]
+    eval_data_dir: Optional[Path]
+    global_path: Optional[Path]  # files_seen36.txt
+    theta_gpu_path: Optional[Path]
+
+    fraction: float = 0.2
+    last_n_files: int = 1
+    last_n_files_eval: int = 1
+    batch_size: int = 1
+    input_shape: List[int] = [1, 32, 32]
+    itemsize: int = 1
+    mixed_precision: bool = True
+    h5_shape: List[int] = [1, 36, 36]
+    tfrecord_shape: List[int] = [1, 36, 36]
+    samples_per_file: int = 1
+
+    # Model params
+    enc_conv_kernels: List[int] = [5, 5, 5, 5]
+    # Encoder filters define OUTPUT filters per layer
+    enc_conv_filters: List[int] = [100, 100, 100, 100]  # 64, 64, 64, 32
+    enc_conv_strides: List[int] = [1, 1, 2, 1]
+    dec_conv_kernels: List[int] = [5, 5, 5, 5]
+    # Decoder filters define INPUT filters per layer
+    dec_conv_filters: List[int] = [100, 100, 100, 100]
+    dec_conv_strides: List[int] = [1, 2, 1, 1]
+    dense_units: int = 64  # 128
+    latent_ndim: int = 10  # 3
+    mixed_precision: bool = True
+    activation: str = "relu"
+    # Setting full_precision_loss to False as we do not support it yet.
+    full_precision_loss: bool = False
+    reconstruction_loss_reduction_type: str = "sum"
+    kl_loss_reduction_type: str = "sum"
+    model_random_seed: Optional[int] = None
+    data_random_seed: Optional[int] = None
+
+    # Optimizer params
+    epsilon: float = 1.0e-8
+    beta1: float = 0.2
+    beta2: float = 0.9
+    decay: float = 0.9
+    momentum: float = 0.9
+    optimizer_name: str = "rmsprop"
+    allowed_optimizers: List[str] = ["sgd", "sgdm", "adam", "rmsprop"]
+    learning_rate: float = 2.0e-5
+    loss_scale: float = 1
+
+    # Logging params are not supported on CS-1 and are disabled in run.py.
+    metrics: bool = True
+
+    # Run params
+    mode: str = "train"
+    model_dir: str = "./model_dir36"
+    train_steps: int = 10
+    eval_steps: int = 2
+    runconfig_params: Optional[str] = None
+    save_checkpoints_steps: int = 10
+    keep_checkpoint_max: int = 3
+    save_summary_steps: int = 10
+    log_step_count_steps: int = 10
 
 
 class ExperimentConfig(BaseSettings):
+    """
+    Master configuration
+    """
     experiment_directory: Path
     md_runner: MDRunnerConfig
     outlier_detection: OutlierDetectionConfig
     gpu_training: Optional[GPUTrainingConfig]
     cs1_training: Optional[CS1TrainingConfig]
+
 
 def read_yaml_config(fname):
     with open(fname) as fp:
--- sim/driver/experiment_main.py
+++ sim/driver/experiment_main.py
@@ -7,11 +7,59 @@ from mpi_launcher import (
 )
 from config import read_yaml_config, MDConfig
 
+
 def start_md_run(workdir, md_config):
+    workdir.mkdir(exist_ok=True)
+    with NamedTemporaryFile(
+        dir=workdir,
+        delete=False,
+        mode="w",
+        prefix="",
+        suffix=".yml"
+    ) as fp:
+        md_config.dump_yaml(fp)
+
+
+def dispatch_md_runs(manager, config):
+    md_runs = []
+
+    md_dir = config.experiment_directory.joinpath("md_runs")
+    local_h5_dir = md_dir.joinpath("h5_contact_maps")
+    md_dir.mkdir()
+    local_h5_dir.mkdir()
+
+    if config.cs1_training is not None:
+        scp_dir
+
+    for i in range(config.num_jobs):
+        workdir = md_dir.joinpath(f"{i:03d}")
+        checkpoint_fname = f"{config.experiment_directory.name}_{i:03d}.chk"
+        md_config = MDConfig(
+            pdb_file=config.md_runner.pdb_file,
+            reference_pdb_file=config.md_runner.reference_pdb_file,
+            top_file=config.md_runner.top_file,
+            checkpoint_file=checkpoint_fname,
+            simulation_length_ns=config.md_runner.simulation_length_ns,
+            report_interval_ps=config.md_runner.report_interval_ps,
+            h5_cp_path=local_h5_dir,
+        )
+    return md_runs
+
 
 def main(config_filename):
     config = read_yaml_config(config_filename)
+
+    config.experiment_directory.mkdir(
+        exist_ok=False  # No duplicate experiment directories!
+    )
+
     manager = ComputeNodeManager()
+    md_runs = dispatch_md_runs(manager, config)
+
+    if config.cs1_training is not None:
+        dispatch_cs1_trainer(config.cs1_training)
+    elif config.gpu_training is not None:
+        dispatch_gpu_trainer(config.gpu_training)
 
     if config.outlier_detection.num_jobs is None:
         config.outlier_detection.num_jobs = 1
@@ -19,5 +67,7 @@ def main(config_filename):
     if config.md_runner.num_jobs is None:
         config.md_runner.num_jobs = manager.num_nodes
 
+
 if __name__ == "__main__":
-    main(sys.argv[1])
+    config_filename = sys.argv[1]
+    main(config_filename)
