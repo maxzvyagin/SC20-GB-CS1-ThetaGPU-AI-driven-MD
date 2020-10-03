5f76286d6464a30dbf7204b5e98649aa6912a67c
braceal <braceal@umich.edu>
2020-10-03 13:17:30 -0400

    initial code. work in progress

---
+++ sim/MD_exps/MD_utils/utils.py
@@ -0,0 +1,18 @@
+import os 
+
+def create_md_path(label, h5_dir=None): 
+    """
+    create MD simulation path based on its label (int), 
+    and automatically update label if path exists. 
+    """
+    md_path = f'omm_runs_{label}'
+
+    try:
+        os.mkdir(md_path)
+
+        if isinstance(h5_dir, str) and not h5_dir.endswith('.h5'):
+            os.mkdir(os.path.join(md_path, h5_dir))
+
+        return md_path
+    except: 
+        return create_md_path(label + 1, h5_dir)
---
+++ sim/MD_exps/run_openmm.py
@@ -0,0 +1,51 @@
+import sys, os 
+import argparse 
+import simtk.unit as u
+from MD_utils.openmm_reporter import CopySender 
+from MD_utils.openmm_simulation import openmm_simulate_amber_implicit
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-cm", "--output_cm", default="output_cm.h5",
+                    help="If name ends with .h5 will write a single HDF5 file." \
+                         "Otherwise will write many with this name prefix")
+parser.add_argument('-c', '--config', help='YAML config file')
+args = parser.parse_args()
+
+cfg = args.config['md_config']
+
+# Handle optional parameters
+if not cfg['top_file']:
+    cfg['top_file'] = None
+
+if not cfg['checkpoint_file']:
+    cfg['checkpoint_file'] =  None
+
+# 'medulla1.cels.anl.gov:/data/shared/vishal/new_dataV2/'
+
+# Configure senders for copying data to local and remote
+senders = []
+if cfg['scp_path']:
+    # Send HDF5 files to medulla
+    # Requires user to execute "export MEDULLA_IDENTITY_FILE=~/.ssh/my-identity-file"
+    # identity_file = os.environ['MEDULLA_IDENTITY_FILE']
+    # method = f'scp -i {identity_file}'
+    method = 'scp'
+    senders.append(CopySender(cfg['scp_path'], method=method))
+
+if cfg['cp_path']:
+    # Send HDF5 files to any local path
+    senders.append(CopySender(cfg['cp_path'], method='cp'))
+
+# check_point = None
+openmm_simulate_amber_implicit(
+        cfg['pdb_file'],
+        top_file=cfg['top_file'],
+        check_point=cfg['check_point'],
+        GPU_index=0,
+        output_traj="output.dcd",
+        output_log="output.log",
+        output_cm=args.output_cm,
+        report_time=float(cfg['report_interval']) * u.picoseconds,
+        sim_time=float(cfg['simulation_length']) * u.nanoseconds, 
+        reeval_time=10*u.nanoseconds,
+        senders=senders)
