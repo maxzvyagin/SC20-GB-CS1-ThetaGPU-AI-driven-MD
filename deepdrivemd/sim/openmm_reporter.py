a34fa6106de22f54b2e51992a6920905f5fdf23a
braceal <braceal@umich.edu>
2020-10-06 23:49:52 -0400

    added rmsd computation in reporter

--- deepdrivemd/sim/openmm_reporter.py
+++ deepdrivemd/sim/openmm_reporter.py
@@ -4,11 +4,11 @@ import logging
 import numpy as np
 import simtk.unit as u
 import MDAnalysis as mda
-from MDAnalysis.analysis import distances
+from MDAnalysis.analysis import distances, rms
 
 logger = logging.getLogger(__name__)
 
-def write_contact_map_h5(file_name, rows, cols):
+def write_contact_map_h5(h5_file, rows, cols):
 
     # Helper function to create ragged array
     def ragged(data):
@@ -19,16 +19,24 @@ def write_contact_map_h5(file_name, rows, cols):
     # Specify variable length arrays
     dt = h5py.vlen_dtype(np.dtype("int16"))
 
-    with h5py.File(file_name, "w", swmr=False) as h5_file:
-        # list of np arrays of shape (2 * X) where X varies
-        data = ragged([np.concatenate(row_col) for row_col in zip(rows, cols)])
-        h5_file.create_dataset(
-            "contact_map",
-            data=data,
-            chunks=(1,) + data.shape[1:],
-            dtype=dt,
-            fletcher32=True,
-        )
+    # list of np arrays of shape (2 * X) where X varies
+    data = ragged([np.concatenate(row_col) for row_col in zip(rows, cols)])
+    h5_file.create_dataset(
+        "contact_map",
+        data=data,
+        dtype=dt,
+        fletcher32=True,
+        chunks=(1,) + data.shape[1:]
+    )
+
+def write_rmsd(h5_file, rmsd):
+    h5_file.create_dataset(
+        "rmsd",
+        data=rmsd,
+        dtype="float16",
+        fletcher=True,
+        chunks=(1,)
+    )
 
 
 class SparseContactMapReporter:
@@ -36,7 +44,7 @@ class SparseContactMapReporter:
         self,
         file,
         reportInterval,
-        reference_pdb_file,
+        reference_pdb_file=None,
         selection="CA",
         threshold=8.0,
         batch_size=2,  # 1024,
@@ -54,11 +62,15 @@ class SparseContactMapReporter:
 
         self._init_batch()
 
-    def _init_reference(self):
-        u_ref = mda.Universe(self._reference_pdb_file)
-        atoms = u_ref.select_atoms("name CA and protein")
-        center = atoms.center(weights=atoms.masses)
-        # https://docs.mdanalysis.org/1.0.0/documentation_pages/analysis/align.html#MDAnalysis.analysis.align._fit_to
+        # Set up for reporting optional RMSD to reference state
+        if reference_pdb_file is not None:
+            u = mda.Universe(self._reference_pdb_file)
+            # Convert openmm atom selection to mda
+            selection = f"protein and name {self._selection}"
+            self._reference_positions = u.select_atoms(selection).positions.copy()
+            self._rmsd = []
+        else:
+            self._reference_positions = None
 
     def _init_batch(self):
         # Frame counter for writing batches to HDF5
@@ -70,7 +82,15 @@ class SparseContactMapReporter:
         steps = self._report_interval - simulation.currentStep % self._report_interval
         return (steps, True, False, False, False, None)
 
-    def _report_contact_maps(self, positions):
+    def _collect_rmsd(self, positions):
+        rmsd = rms.rmsd(
+            positions,
+            self._reference_positions,
+            superposition=True
+        )
+        self._rmsd.append(rmsd)
+
+    def _collect_contact_maps(self, positions):
 
         contact_map = distances.contact_matrix(
             positions, self._threshold, returntype="sparse"
@@ -88,13 +108,23 @@ class SparseContactMapReporter:
         all_positions = np.array(state.getPositions().value_in_unit(u.angstrom))
         positions = all_positions[atom_indices].astype(np.float32)
 
-        self._report_contact_maps(positions)
+        self._collect_contact_maps(positions)
+
+        if self._reference_positions is not None:
+            self._collect_rmsd(positions)
 
         self._num_frames += 1
 
         if self._num_frames == self._batch_size:
             file_name = f"{self._base_name}_{self._file_idx:05d}_{uuid.uuid4()}.h5"
-            write_contact_map_h5(file_name, self._rows, self._cols)
+
+            with h5py.File(file_name, "w", swmr=False) as h5_file:
+                write_contact_map_h5(h5_file, self._rows, self._cols)
+                # Optionally, write rmsd to the reference state
+                if self._reference_positions is not None:
+                    write_rmsd(h5_file, self._rmsd)
+                    self._rmsd = []
+
             logger.debug(f"H5 file written with {len(self._rows)} frames.")
             self._init_batch()
             self._file_idx += 1
