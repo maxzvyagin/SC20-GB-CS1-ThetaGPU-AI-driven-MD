6a22814ef267c6c86f37e29bf9cecbd8a1a9fbe5
msalim <msalim@anl.gov>
2020-10-07 17:24:05 -0500

    add production inputs

---
+++ production-inputs/submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#COBALT -n 21
+#COBALT -t 150
+#COBALT -q R.CVD-Mol-AI
+#COBALT -A CVD-Mol-AI
+#COBALT --attrs pubnet=true:enable_ssh=1:ssds=required:ssd_size=2048
+
+source /lus/theta-fs0/projects/RL-fold/msalim/SC20-GB-CS1-ThetaGPU-AI-driven-MD/env/bin/activate
+
+python -m deepdrivemd.driver.experiment_main $1
