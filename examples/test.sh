4cd4e962ddee2c17fe6101dd09f98af117f8f6b2
msalim <msalim@anl.gov>
2020-10-07 19:46:23 +0000

    fix experiment_main cs1 launch

--- examples/test.sh
+++ examples/test.sh
@@ -6,7 +6,5 @@
 #COBALT --attrs pubnet=true:enable_ssh=1:ssds=required:ssd_size=2048
 
 source /lus/theta-fs0/projects/RL-fold/msalim/SC20-GB-CS1-ThetaGPU-AI-driven-MD/env/bin/activate
-mpirun -n 2 -npernode 1 rm -r /raid/scratch/*
-rm -r /home/msalim/RL-fold/msalim/test-mdonly
 
-python -m deepdrivemd.driver.experiment_main /home/msalim/RL-fold/msalim/SC20-GB-CS1-ThetaGPU-AI-driven-MD/examples/test-md-only.yaml
+python -m deepdrivemd.driver.experiment_main /home/msalim/RL-fold/msalim/SC20-GB-CS1-ThetaGPU-AI-driven-MD/examples/test-full.yaml
