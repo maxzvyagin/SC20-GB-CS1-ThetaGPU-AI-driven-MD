39d54d42e4adad7f03a31e7fee18c0d479de3a7c
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-11-09 15:03:30 -0700

    mixed preciion fix

--- deepdrivemd/models/symmetric_cvae/model.py
+++ deepdrivemd/models/symmetric_cvae/model.py
@@ -30,7 +30,7 @@ def model_fn(features, labels, mode, params):
     global_step = tf.compat.v1.train.get_global_step()
 
     if mixed_precision:
-        tf.keras.mixed_precision.experimental.set_policy("mixed_float16")
+        tf.keras.mixed_precision.set_global_policy('mixed_float16')
 
     # Model output
     outputs, kl_loss, embedding_output = build_model(features, params)
