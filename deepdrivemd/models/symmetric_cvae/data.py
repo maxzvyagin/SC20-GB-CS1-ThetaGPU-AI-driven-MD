71a4db58df1835731f27d3fece2f61707d68c69d
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-11-10 09:36:26 -0700

    old code

--- deepdrivemd/models/symmetric_cvae/data.py
+++ deepdrivemd/models/symmetric_cvae/data.py
@@ -1,10 +1,234 @@
+# """
+# File containing input data functions used for the Covid CVAE model.
+# """
+# import tensorflow as tf
+# import numpy as np
+# import glob
+# import pdb
+#
+#
+# def get_real_datasets(data_dir):
+#     train_ds = tf.data.TFRecordDataset(f"{data_dir}/train.tfrecords")
+#     val_ds = tf.data.TFRecordDataset(f"{data_dir}/val.tfrecords")
+#     return train_ds, val_ds
+#
+#
+# def get_real_batch_map_fn(tfrecord_shape, input_shape, dtype):
+#     feature_description = {
+#         "data": tf.io.FixedLenFeature([1], tf.string),
+#     }
+#
+#     def _map_fn(record):
+#         features = tf.io.parse_example(record, feature_description)
+#         data = tf.compat.v1.io.decode_raw(features["data"], tf.bool,)
+#         pdb.set_trace()
+#         BS = 1
+#         x = tf.reshape(data, [BS] + tfrecord_shape)
+#         input_x = tf.slice(x, [0, 0, 0, 0], [BS] + input_shape)
+#         flat_input_x = tf.reshape(input_x, [BS, -1])
+#         return (tf.cast(input_x, dtype), tf.cast(flat_input_x, dtype))
+#
+#     return _map_fn
+#
+#
+# def train_input_fn(params):
+#     return input_fn(params, "train")
+#
+#
+# def val_input_fn(params):
+#     return input_fn(params, "val")
+#
+#
+# def input_fn(params, split):
+#     batch_size = params["batch_size"]
+#     input_shape = params["input_shape"]
+#     mp = params["mixed_precision"]
+#     dtype = tf.float16 if mp else tf.float32
+#     data_dir = params["data_dir"]
+#     tfrecord_shape = params["tfrecord_shape"]
+#     seed = params["data_random_seed"]
+#
+#     train_ds, val_ds = get_real_datasets(data_dir)
+#     map_fn = get_real_batch_map_fn(tfrecord_shape, input_shape, dtype)
+#
+#     if split == "train":
+#         ds = train_ds
+#     elif split == "val":
+#         ds = val_ds
+#         batch_size = 1
+#     else:
+#         raise ValueError(f"Invalid split: {split}")
+#
+#     if split == "train":
+#         ds = ds.repeat()
+#         ds = ds.shuffle(1000, seed=seed)
+#     ds = ds.batch(batch_size, drop_remainder=True)
+#     ds = ds.map(map_fn, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+#     ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
+#     return ds
+#
+#
+# ##########################################
+#
+#
+# # using npy
+# def parse_funct(input_shape, final_shape, dtype):
+#     def _parse_sample(raw_record):
+#         # dtype should match what was used in utils.py
+#         feature = tf.io.decode_raw(raw_record, tf.bool)
+#         batch_size = feature.shape.as_list()[0]
+#         image = tf.reshape(feature, [batch_size] + input_shape)
+#         act_img = tf.slice(image, [0, 0, 0, 0], [batch_size] + final_shape)
+#         flat_img = tf.reshape(act_img, [batch_size, -1])
+#         return tf.cast(act_img, dtype), tf.cast(flat_img, dtype)
+#
+#     return _parse_sample
+#
+#
+# def simulation_input_fn(params):
+#     batch_size = params["batch_size"]
+#     input_shape = params["tfrecord_shape"]
+#     final_shape = params["input_shape"]
+#     itemsize = params["itemsize"]
+#     mp = params["mixed_precision"]
+#     dtype = tf.float16 if mp else tf.float32
+#     data_dir = params["data_dir"]
+#     list_files = glob.glob(f"{data_dir}/*.npy")
+#     assert len(list_files) > 10, f"{len(list_files)}, {data_dir}"
+#     dataset = tf.data.FixedLengthRecordDataset(
+#         list_files,
+#         record_bytes=np.prod(input_shape) * itemsize,
+#         header_bytes=128,  # npy usual but can change
+#     )
+#     dataset = dataset.batch(batch_size, drop_remainder=True)
+#     parse_sample = parse_funct(input_shape, final_shape, dtype)
+#     dataset = dataset.map(parse_sample)
+#     dataset = dataset.shuffle(10,)
+#     dataset = dataset.repeat()
+#     dataset = dataset.prefetch(2)
+#     return dataset
+#
+#
+# # tfrecords
+#
+#
+# def parse_function_record(dtype, input_shape, final_shape):
+#     feature_description = {
+#         "image_raw": tf.io.FixedLenFeature([1], tf.string),
+#     }
+#
+#     def _parse_record(record):
+#         features = tf.io.parse_example(record, features=feature_description)
+#         # dtype should match what was used in utils.py
+#         image = tf.io.decode_raw(features["image_raw"], tf.float16)
+#         batch_size = image.shape.as_list()[0]
+#         image = tf.reshape(image, [batch_size] + input_shape)
+#         act_img = tf.slice(image, [0, 0, 0, 0], [batch_size] + final_shape)
+#         flat_img = tf.reshape(act_img, [batch_size, -1])
+#         return tf.cast(act_img, dtype), tf.cast(flat_img, dtype)
+#
+#     return _parse_record
+#
+#
+# def parse_function_record_predict(dtype, input_shape, final_shape):
+#     feature_description = {
+#         "image_raw": tf.io.FixedLenFeature([1], tf.string),
+#     }
+#
+#     def _parse_record(record):
+#         features = tf.io.parse_example(record, features=feature_description)
+#         # dtype should match what was used in utils.py
+#         image = tf.io.decode_raw(features["image_raw"], tf.float16)
+#         batch_size = image.shape.as_list()[0]
+#         image = tf.reshape(image, [batch_size] + input_shape)
+#         act_img = tf.slice(image, [0, 0, 0, 0], [batch_size] + final_shape)
+#         return tf.cast(act_img, dtype)
+#
+#     return _parse_record
+#
+#
+# def sort_string_numbers(filename):
+#     part2 = ".tfrecords"
+#     part1 = "_sample"
+#     num1 = filename.find(part1) + len(part1)
+#     num2 = filename.find(part2)
+#     return int(filename[num1:num2])
+#
+#
+# def simulation_tf_record_input_fn(params):
+#     batch_size = params["batch_size"]
+#     input_shape = params["tfrecord_shape"]
+#     final_shape = params["input_shape"]
+#     mp = params["mixed_precision"]
+#     dtype = tf.float16 if mp else tf.float32
+#     data_dir = params["data_dir"]
+#     last_n_files = params["last_n_files"]
+#     files = glob.glob(f"{data_dir}/*.tfrecords")
+#     last_n_files = sorted(files, key=sort_string_numbers)[:last_n_files]
+#     list_files = tf.data.Dataset.list_files(last_n_files)
+#     dataset = tf.data.TFRecordDataset(list_files)
+#     dataset = dataset.batch(batch_size, drop_remainder=True)
+#     parse_sample = parse_function_record(dtype, input_shape, final_shape)
+#     dataset = dataset.map(parse_sample)
+#     dataset = dataset.shuffle(10,)
+#     dataset = dataset.repeat()
+#     dataset = dataset.prefetch(2)
+#     return dataset
+#
+#
+# def simulation_tf_record_eval_input_fn(params):
+#     batch_size = params["batch_size"]
+#     input_shape = params["tfrecord_shape"]
+#     final_shape = params["input_shape"]
+#     mp = params["mixed_precision"]
+#     dtype = tf.float16 if mp else tf.float32
+#     data_dir = params["eval_data_dir"]
+#     last_n_files = params["last_n_files_eval"]
+#     files = glob.glob(f"{data_dir}/*.tfrecords")
+#     last_n_files = sorted(files, key=sort_string_numbers)[:last_n_files]
+#     list_files = tf.data.Dataset.list_files(last_n_files)
+#     dataset = tf.data.TFRecordDataset(list_files)
+#     dataset = dataset.batch(batch_size, drop_remainder=True)
+#     parse_sample = parse_function_record(dtype, input_shape, final_shape)
+#     dataset = dataset.map(parse_sample)
+#     dataset = dataset.shuffle(10,)
+#     dataset = dataset.repeat()
+#     dataset = dataset.prefetch(2)
+#     return dataset
+#
+#
+# if __name__ == "__main__":
+#     for i in range(2):
+#         params = {
+#             "batch_size": 4,
+#             "tfrecord_shape": [1, 256, 256],
+#             "itemsize": 1,
+#             "mixed_precision": False,
+#             "data_dir": "/cb/home/vishal/ws/ANL-shared/cvae_gb/records_1",
+#             "input_shape": [1, 192, 192],
+#         }
+#         if i == 0:
+#             ds = simulation_tf_record_input_fn(params)
+#         else:
+#             params["data_dir"] = "/cb/home/vishal/ws/ANL-shared/cvae_gb/npy_data_256"
+#             ds = simulation_input_fn(params)
+#         print(ds)
+#         dataset_iterator = tf.compat.v1.data.make_one_shot_iterator(ds)
+#         inputs = dataset_iterator.get_next()
+#         print(inputs)
+#         with tf.compat.v1.Session() as sess:
+#             sess.run(tf.compat.v1.tables_initializer())
+#             for i in range(10):
+#                 outputs = sess.run(inputs)
+#                 print(outputs[0].shape, outputs[1].shape)
+
+
 """
 File containing input data functions used for the Covid CVAE model.
 """
 import tensorflow as tf
 import numpy as np
 import glob
-import pdb
 
 
 def get_real_datasets(data_dir):
@@ -21,8 +245,7 @@ def get_real_batch_map_fn(tfrecord_shape, input_shape, dtype):
     def _map_fn(record):
         features = tf.io.parse_example(record, feature_description)
         data = tf.compat.v1.io.decode_raw(features["data"], tf.bool,)
-        pdb.set_trace()
-        BS = 1
+        BS = data.shape.as_list()[0]
         x = tf.reshape(data, [BS] + tfrecord_shape)
         input_x = tf.slice(x, [0, 0, 0, 0], [BS] + input_shape)
         flat_input_x = tf.reshape(input_x, [BS, -1])
@@ -105,7 +328,7 @@ def simulation_input_fn(params):
     dataset = dataset.map(parse_sample)
     dataset = dataset.shuffle(10,)
     dataset = dataset.repeat()
-    dataset = dataset.prefetch(2)
+    dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
     return dataset
 
 
@@ -118,9 +341,9 @@ def parse_function_record(dtype, input_shape, final_shape):
     }
 
     def _parse_record(record):
-        features = tf.io.parse_example(record, features=feature_description)
+        features = tf.parse_example(record, features=feature_description)
         # dtype should match what was used in utils.py
-        image = tf.io.decode_raw(features["image_raw"], tf.float16)
+        image = tf.decode_raw(features["image_raw"], tf.float16)
         batch_size = image.shape.as_list()[0]
         image = tf.reshape(image, [batch_size] + input_shape)
         act_img = tf.slice(image, [0, 0, 0, 0], [batch_size] + final_shape)
@@ -136,9 +359,9 @@ def parse_function_record_predict(dtype, input_shape, final_shape):
     }
 
     def _parse_record(record):
-        features = tf.io.parse_example(record, features=feature_description)
+        features = tf.parse_example(record, features=feature_description)
         # dtype should match what was used in utils.py
-        image = tf.io.decode_raw(features["image_raw"], tf.float16)
+        image = tf.decode_raw(features["image_raw"], tf.float16)
         batch_size = image.shape.as_list()[0]
         image = tf.reshape(image, [batch_size] + input_shape)
         act_img = tf.slice(image, [0, 0, 0, 0], [batch_size] + final_shape)
@@ -172,7 +395,7 @@ def simulation_tf_record_input_fn(params):
     dataset = dataset.map(parse_sample)
     dataset = dataset.shuffle(10,)
     dataset = dataset.repeat()
-    dataset = dataset.prefetch(2)
+    dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
     return dataset
 
 
@@ -193,7 +416,7 @@ def simulation_tf_record_eval_input_fn(params):
     dataset = dataset.map(parse_sample)
     dataset = dataset.shuffle(10,)
     dataset = dataset.repeat()
-    dataset = dataset.prefetch(2)
+    dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
     return dataset
 
 
--- deepdrivemd/models/symmetric_cvae/utils.py
+++ deepdrivemd/models/symmetric_cvae/utils.py
@@ -1,3 +1,164 @@
+# import tensorflow as tf
+# from pathlib import Path
+# import h5py
+# import numpy as np
+# from scipy.sparse import coo_matrix
+# import logging
+# import os
+# import glob
+# # import yaml
+# import shutil
+# import random
+# from tqdm import tqdm
+#
+# logger = logging.getLogger(__name__)
+#
+# # _curdir = os.path.dirname(os.path.abspath(__file__))
+# # DEFAULT_YAML_PATH = os.path.join(_curdir, "params.yaml")
+# DEFAULT_YAML_PATH = "params.yaml"
+#
+#
+# def get_params(params_file=DEFAULT_YAML_PATH):
+#     """
+#     Return params dict from yaml file.
+#     """
+#     with open(params_file, "r") as stream:
+#         params = yaml.safe_load(stream)
+#     return params
+#
+#
+# def preprocess_h5files(files, shape):
+#     os.makedirs("./npy_data_256", exist_ok=True)
+#     for i, h5_file_path in enumerate(files):
+#         with h5py.File(h5_file_path, "r", libver="latest", swmr=False) as f:
+#             for j, raw_indices in enumerate(f["contact_map"]):
+#                 # Unpack indices (stored as concatenated row, col vectors)
+#                 indices = raw_indices.reshape((2, -1)).astype("int16")
+#                 # Contact matrices are binary so we don't need to store the values
+#                 # in HDF5 format. Instead we create a vector of 1s on the fly.
+#                 values = np.ones(indices.shape[1]).astype("byte")
+#                 # Construct COO formated sparse matrix
+#                 contact_map = coo_matrix(
+#                     (values, (indices[0], indices[1])), shape=shape
+#                 ).todense()
+#                 contact_map = contact_map[:256, :256]
+#                 # Expand COO format to dense and add matrix to return array
+#                 np.save(
+#                     f"npy_data_256/data_file_{i+1}_sample{j+1}.npy",
+#                     np.array(contact_map, dtype=np.float16),
+#                 )
+#
+#
+# def _bytes_feature(value):
+#     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+#
+#
+# def write_record(contact_maps, record_name):
+#     with tf.io.TFRecordWriter(record_name) as writer:
+#         for sample in contact_maps:
+#             feature = {"image_raw": _bytes_feature(sample.tobytes())}
+#             features = tf.train.Features(feature=feature)
+#             example = tf.train.Example(features=features)
+#             example_to_string = example.SerializeToString()
+#             writer.write(example_to_string)
+#
+#
+# def write_single_tfrecord(h5_file, initial_shape, final_shape, tfrecord_dir):
+#     """
+#     Special-case: 1-to-1 mapping of h5_file to .tfrecords for lof.py
+#     Not needed on CS1
+#     """
+#     tfrecord_dir = Path(tfrecord_dir)
+#     record_name = tfrecord_dir.joinpath(
+#         Path(h5_file).with_suffix(".tfrecords").name
+#     ).as_posix()
+#     contact_maps = []
+#     with h5py.File(h5_file, "r", libver="latest", swmr=False) as f:
+#         for j, raw_indices in enumerate(f["contact_map"]):
+#             indices = raw_indices.reshape((2, -1)).astype("int16")
+#             # Contact matrices are binary so we don't need to store the values
+#             # in HDF5 format. Instead we create a vector of 1s on the fly.
+#             values = np.ones(indices.shape[1]).astype("byte")
+#             # Construct COO formated sparse matrix
+#             contact_map = coo_matrix(
+#                 (values, (indices[0], indices[1])), shape=initial_shape
+#             ).todense()
+#             contact_map = np.array(
+#                 contact_map[: final_shape[0], : final_shape[1]], dtype=np.float16
+#             )
+#             contact_maps.append(contact_map)
+#         write_record(contact_maps, record_name)
+#         logger.debug(f"Wrote TFRecord: {record_name}")
+#
+#
+# def write_to_tfrecords(
+#     files,
+#     initial_shape,
+#     final_shape,
+#     num_samples,
+#     train_dir_path,
+#     eval_dir_path,
+#     fraction=0.2,
+#     padding=False
+# ):
+#     os.makedirs(train_dir_path, exist_ok=True)
+#     os.makedirs(eval_dir_path, exist_ok=True)
+#     record_counter = len(glob.glob(f"{train_dir_path}/*.tfrecords"))
+#     initial_counter = record_counter
+#     contact_maps = []
+#     for i, h5_file_path in enumerate(files):
+#         with h5py.File(h5_file_path, "r", libver="latest", swmr=False) as f:
+#             for j, raw_indices in tqdm(enumerate(f["contact_map"])):
+#                 indices = raw_indices.reshape((2, -1)).astype("int16")
+#                 # Contact matrices are binary so we don't need to store the values
+#                 # in HDF5 format. Instead we create a vector of 1s on the fly.
+#                 values = np.ones(indices.shape[1]).astype("byte")
+#                 # Construct COO formated sparse matrix
+#                 contact_map = coo_matrix(
+#                     (values, (indices[0], indices[1])), shape=initial_shape
+#                 ).todense()
+#                 if padding:
+#                     contact_map = np.array(
+#                         contact_map[: final_shape[0], : final_shape[1]], dtype=bool
+#                     )
+#                     pad_size = (final_shape[0] - initial_shape[0]) // 2
+#                     contact_map = np.pad(contact_map, 1, mode="constant")
+#                     print(contact_map.shape)
+#                     # print(np.size(contact_map)-np.count_nonzero(contact_map))
+#                 else:
+#                     contact_map = np.array(
+#                         contact_map[: final_shape[0], : final_shape[1]], dtype=bool
+#                     )
+#                 contact_maps.append(contact_map)
+#                 if len(contact_maps) == num_samples:
+#                     record_counter += 1
+#                     record_name = os.path.join(
+#                         train_dir_path,
+#                         f"tfrecord_{i}_sample{record_counter}.tfrecords",
+#                     )
+#                     logger.debug(f"Wrote TFRecord: {record_name}")
+#                     write_record(contact_maps, record_name)
+#                     contact_maps = []
+#     if len(contact_maps) > 0:
+#         record_counter += 1
+#         record_name = os.path.join(
+#             train_dir_path, f"tfrecord_{i}_sample{record_counter}.tfrecords"
+#         )
+#         write_record(contact_maps, record_name)
+#         contact_maps = []
+#     files_created = record_counter - initial_counter
+#     # choose X% between initial_counter to record_counter to move to eval_dir_path
+#     randomlist = random.sample(
+#         range(initial_counter + 1, record_counter + 1), int(fraction * files_created)
+#     )
+#     for n in randomlist:
+#         full_filename = glob.glob(f"{train_dir_path}/*_sample{n}.tfrecords")[0]
+#         file_name = full_filename[
+#             full_filename.find(f"{train_dir_path}/") + len(f"{train_dir_path}/") :
+#         ]
+#         shutil.move(full_filename, f"{eval_dir_path}/{file_name}")
+
+
 import tensorflow as tf
 from pathlib import Path
 import h5py
@@ -6,10 +167,9 @@ from scipy.sparse import coo_matrix
 import logging
 import os
 import glob
-# import yaml
+import yaml
 import shutil
 import random
-from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -54,9 +214,9 @@ def _bytes_feature(value):
 
 
 def write_record(contact_maps, record_name):
-    with tf.io.TFRecordWriter(record_name) as writer:
+    with tf.python_io.TFRecordWriter(record_name) as writer:
         for sample in contact_maps:
-            feature = {"image_raw": _bytes_feature(sample.tobytes())}
+            feature = {"image_raw": _bytes_feature(sample.tostring())}
             features = tf.train.Features(feature=feature)
             example = tf.train.Example(features=features)
             example_to_string = example.SerializeToString()
@@ -99,7 +259,6 @@ def write_to_tfrecords(
     train_dir_path,
     eval_dir_path,
     fraction=0.2,
-    padding=False
 ):
     os.makedirs(train_dir_path, exist_ok=True)
     os.makedirs(eval_dir_path, exist_ok=True)
@@ -108,7 +267,7 @@ def write_to_tfrecords(
     contact_maps = []
     for i, h5_file_path in enumerate(files):
         with h5py.File(h5_file_path, "r", libver="latest", swmr=False) as f:
-            for j, raw_indices in tqdm(enumerate(f["contact_map"])):
+            for j, raw_indices in enumerate(f["contact_map"]):
                 indices = raw_indices.reshape((2, -1)).astype("int16")
                 # Contact matrices are binary so we don't need to store the values
                 # in HDF5 format. Instead we create a vector of 1s on the fly.
@@ -117,18 +276,9 @@ def write_to_tfrecords(
                 contact_map = coo_matrix(
                     (values, (indices[0], indices[1])), shape=initial_shape
                 ).todense()
-                if padding:
-                    contact_map = np.array(
-                        contact_map[: final_shape[0], : final_shape[1]], dtype=bool
-                    )
-                    pad_size = (final_shape[0] - initial_shape[0]) // 2
-                    contact_map = np.pad(contact_map, 1, mode="constant")
-                    print(contact_map.shape)
-                    # print(np.size(contact_map)-np.count_nonzero(contact_map))
-                else:
-                    contact_map = np.array(
-                        contact_map[: final_shape[0], : final_shape[1]], dtype=bool
-                    )
+                contact_map = np.array(
+                    contact_map[: final_shape[0], : final_shape[1]], dtype=np.float16
+                )
                 contact_maps.append(contact_map)
                 if len(contact_maps) == num_samples:
                     record_counter += 1
@@ -157,6 +307,7 @@ def write_to_tfrecords(
             full_filename.find(f"{train_dir_path}/") + len(f"{train_dir_path}/") :
         ]
         shutil.move(full_filename, f"{eval_dir_path}/{file_name}")
+
 
 
 if __name__ == "__main__":
